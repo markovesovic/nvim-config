3c0437de704b79f3c7dab4a0649822274017e5f7
Mare Care <mare.vesovic@gmail.com>
2023-01-02 01:35:15 +0100

    Initial plugin configuration

---
+++ init.lua
@@ -0,0 +1,7 @@
+require "marecare.plugins"
+
+require "marecare.neoscroll"
+require "marecare.options"
+require "marecare.keymaps"
+require "marecare.colorscheme"
+require "marecare.cmp"
---
+++ lua/marecare/plugins.lua
@@ -0,0 +1,97 @@
+local fn = vim.fn
+
+--[[
+--  Install packer automatically
+--]]
+
+local install_path = fn.stdpath "data" .. "site/pack/packer/start/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.vim",
+    install_path,
+  }
+  print "Installing packer: close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+
+--------
+
+--[[
+--  Autocommand that reloads neovim whenever you write plugins.lua file"
+--]]
+
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+
+--------
+
+--[[
+--  Initialize packer
+--]]
+
+local ok, packer = pcall(require, "packer")
+
+if not ok then
+  vim.notify("Failed to initialize packer. Exiting...")
+  return
+end
+
+packer.init {
+  display = {
+    open_fn = function() 
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+------
+
+--[[
+--  Specify plugins for installation
+--]]
+
+
+return packer.startup(
+  function (use)
+
+    use "wbthomason/packer.nvim" -- Have packer manage itself
+    use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+    use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+
+    -- Colorschemes
+    use "lunarvim/colorschemes"
+    use "lunarvim/darkplus.nvim"
+    use 'ful1e5/onedark.nvim'
+
+    -- cmp plugins
+    use "hrsh7th/nvim-cmp" -- The completion plugin
+    use "hrsh7th/cmp-buffer" -- buffer completions
+    use "hrsh7th/cmp-path" -- path completions
+    use "hrsh7th/cmp-cmdline" -- cmdline completions
+    use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+    -- snippets
+    use "L3MON4D3/LuaSnip" --snippet engine
+    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+    use "karb94/neoscroll.nvim" -- smooth scroll
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    if PACKER_BOOTSTRAP then
+      require("packer").sync()
+    end
+  end
+)
+
+
